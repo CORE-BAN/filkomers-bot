f301080c7cc72d568746bfacfcc689a981a3c7a0
Sulthan Abiyyu <zblackjack980@gmail.com>
2022-07-29 17:00:38 +0700

    todo: get berita

---
+++ scrape/berita_scrape.py
@@ -0,0 +1,9 @@
+from bs4 import BeautifulSoup
+import requests
+import os
+import json
+
+
+def get_berita():
+    # TODO: scraping berita https://filkom.ub.ac.id/berita/
+    print("berita terkini")
